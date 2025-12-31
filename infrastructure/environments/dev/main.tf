70688fbdc5a5fa806e292e442c7cdb94bd77017f
Baris Top <baris1892@users.noreply.github.com>
2025-12-31 17:30:00 +0100

    Revert "refactored terraform files using "shared" directory"
    This reverts commit 39b5c0909a53cb88c286b06a35eede3874ec7adf.

--- infrastructure/environments/dev/main.tf
+++ infrastructure/environments/dev/main.tf
@@ -8,25 +8,69 @@ resource "kubernetes_namespace" "this" {
   }
 }
 
+resource "helm_release" "frontend" {
+  name      = "frontend"
+  chart     = "../../charts/frontend"
+  namespace = var.namespace
+  values = [
+    file("../../charts/frontend/values.yaml"),
+    file("../../charts/frontend/values-dev.yaml")
+  ]
+}
+
+resource "helm_release" "backend" {
+  name      = "backend"
+  chart     = "../../charts/backend"
+  namespace = var.namespace
+  values = [
+    file("../../charts/backend/values.yaml"),
+    file("../../charts/backend/values-dev.yaml")
+  ]
+}
+
+# SOPS secrets directly in root module
 data "sops_file" "database" {
   source_file = "../../charts/database/values-secrets-dev.yaml"
 }
 
-module "app" {
-  source = "../../shared"
-
+resource "helm_release" "database" {
+  name      = "database"
+  chart     = "../../charts/database"
   namespace = var.namespace
+  values    = [file("../../charts/database/values.yaml")]
 
-  frontend_values = [
-    file("../../charts/frontend/values.yaml"),
-    file("../../charts/frontend/values-dev.yaml")
-  ]
+  set {
+    name  = "postgres.username"
+    value = data.sops_file.database.data["postgres.username"]
+  }
 
-  backend_values = [
-    file("../../charts/backend/values.yaml"),
-    file("../../charts/backend/values-dev.yaml")
-  ]
+  set {
+    name  = "postgres.password"
+    value = data.sops_file.database.data["postgres.password"]
+  }
 
-  database_values_file = "../../charts/database/values.yaml"
-  database_secrets     = data.sops_file.database
+  set {
+    name  = "postgres.db"
+    value = data.sops_file.database.data["postgres.db"]
+  }
 }
+
+# TODO: is the above approach fine or should we make it better reusable? not sure...
+
+# module "app" {
+#   source = "../../modules/app"
+#   namespace = var.namespace
+#
+#   frontend_values = [
+#     file("../../charts/frontend/values.yaml"),
+#     file("../../charts/frontend/values-dev.yaml")
+#   ]
+#
+#   backend_values = [
+#     file("../../charts/backend/values.yaml"),
+#     file("../../charts/backend/values-dev.yaml")
+#   ]
+#
+#   database_value   = [file("../../charts/database/values.yaml")]
+#   database_secrets = data.sops_file.database
+# }
