f1d9fb60eff99924c8fd36bb2c28644aaa3e2db7
Baris Top <baris1892@users.noreply.github.com>
2026-01-26 09:20:49 +0100

    updated argocd to a more recent version 9.3.5

--- infrastructure/infra/argocd.tf
+++ infrastructure/infra/argocd.tf
@@ -10,7 +10,7 @@ resource "helm_release" "argocd" {
 
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
-  version    = "5.51.6"
+  version    = "9.3.5"
 
   values = [
     file("${path.module}/argocd-values.yaml")
