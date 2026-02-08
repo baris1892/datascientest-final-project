280df00d32a4f9da1c0124b4f50006bfe99349d4
Baris Top <baris1892@users.noreply.github.com>
2026-02-08 16:05:37 +0100

    added initial loki via helm charts

--- infrastructure/infra/kube-prometheus-stack.tf
+++ infrastructure/infra/kube-prometheus-stack.tf
@@ -128,3 +128,64 @@ resource "kubernetes_manifest" "frontend_probes" {
     }
   }
 }
+
+resource "helm_release" "loki" {
+  name       = "loki"
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "loki"
+  namespace  = kubernetes_namespace.monitoring.metadata[0].name
+  version    = "6.50.0"
+
+  values = [
+    yamlencode({
+      deploymentMode = "SingleBinary"
+
+      loki = {
+        auth_enabled = false
+
+        commonConfig = {
+          replication_factor = 1
+        }
+
+        schemaConfig = {
+          configs = [{
+            from         = "2024-04-01"
+            store        = "tsdb"
+            object_store = "filesystem"
+            schema       = "v13"
+            index = {
+              prefix = "loki_index_"
+              period = "24h"
+            }
+          }]
+        }
+
+        storage = {
+          type = "filesystem"
+        }
+      }
+
+      singleBinary = {
+        replicas = 1
+        persistence = {
+          enabled = true
+          size    = "1Gi"
+        }
+      }
+
+      # Disable all microservices
+      backend = { replicas = 0 }
+      read    = { replicas = 0 }
+      write   = { replicas = 0 }
+
+      gateway = {
+        enabled = true
+      }
+
+      test = {
+        enabled = false
+      }
+    })
+  ]
+}
+
